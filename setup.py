5698cdff05db8c13adfffd21eb34f933b6cda01b
David Szotten <davidszotten@gmail.com>
2016-10-11 12:30:48 +0100

    tweak setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
-setup(name='sentry_stack_checker',
+setup(name='sentry-stack-checker',
       version='0.1',
-      #  description='',
-      #  url='http://github.com/',
+      description='Pylint plugin for checking usage of log.exception',
+      url='https://github.com/davidszotten/sentry-stack-checker',
       author='David Szotten',
       author_email='davidszotten@gmail.com',
       license='MIT',
-      #  packages=['funniest'],
       py_modules=['sentry_stack_checker'],
       zip_safe=False)
